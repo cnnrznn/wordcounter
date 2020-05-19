abee019d5057cdd5446e19172257f878a102abff
Connor Zanin <cnnrznn@gmail.com>
2020-05-19 09:41:38 -0400

    reorganize front-end, handle no url case

--- frontend/main.go
+++ frontend/main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"text/template"
+	"os"
+	"sort"
+
+	. "github.com/cnnrznn/wordcounter/util"
 )
 
 var (
 	tpl *template.Template
 )
 
-type Count struct {
-	Key string
-	Val int
+func main() {
+	t, err := template.New("index").ParseGlob("templates/index.tpl")
+	if err != nil {
+		log.Fatal("Failed to parse templates: ", err)
+	}
+	tpl = t
+
+	// dynamic content
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/post", handlePost)
+
+	// static file server
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func doBackendReq(URL *url.URL, wcr *WCResponse) error {
+	resp, err := http.Get(fmt.Sprintf("http://%s/wordcount?url=%s",
+		os.Getenv("BACKEND_API_ADDR"),
+		url.QueryEscape(URL.String())))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, wcr); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func handleIndex(w http.ResponseWriter, req *http.Request) {
@@ -28,17 +69,31 @@ func handleIndex(w http.ResponseWriter, req *http.Request) {
 	// check url is valid
 	URL, err := url.Parse(req.URL.Query().Get("url"))
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "Not a valid url", http.StatusBadRequest)
 		return
 	}
 
-	wordcount := []Count{}
+	wcr := WCResponse{}
+
+	if len(URL.String()) > 0 {
+		if err := doBackendReq(URL, &wcr); err != nil {
+			http.Error(w, "Backend request failed", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// order wordcounts descending
+	sort.Slice(wcr.Wordcount, func(i, j int) bool {
+		if wcr.Wordcount[i].Val > wcr.Wordcount[j].Val {
+			return true
+		}
+		return false
+	})
 
 	err = tpl.ExecuteTemplate(w, "index", map[string]interface{}{
-		"wordcount": wordcount,
-		"Time":      0,
-		"URL":       URL,
+		"Wordcount": wcr.Wordcount,
+		"Time":      wcr.Time,
+		"URL":       wcr.URL,
 	})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -63,25 +118,6 @@ func handlePost(w http.ResponseWriter, req *http.Request) {
 	q := u.Query()
 	q.Set("url", URL)
 	u.RawQuery = q.Encode()
-	log.Println(u.String())
 
 	http.Redirect(w, req, u.String(), http.StatusFound)
-}
-
-func main() {
-	t, err := template.New("index").ParseGlob("templates/index.tpl")
-	if err != nil {
-		log.Fatal("Failed to parse templates: ", err)
-	}
-	tpl = t
-
-	// dynamic content
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/post", handlePost)
-
-	// static file server
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
